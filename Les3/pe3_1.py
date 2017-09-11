fa666a04dc3a4228cec1d6f18f58039e5e725285
eline <eline.nube@student.hu.nl>
2017-09-11 11:22:49 +0200

    start

---
+++ Les3/pe3_1.py
@@ -0,0 +1,5 @@
+score = eval(input('Geef je score'))
+if score > 15:
+    print('Gefeliciteerd!')
+
+print('Met een score van minimaal 15 ben je geslaagd!')
---
+++ Les3/pe3_4.py
@@ -0,0 +1,1 @@
+dagen = ['maandag', 'dinsdag', 'woensdag']
